175a7ca42594affca9c61fc9c670921c21c0ecb4
Aya Y <ayay@Ayas-MacBook-Pro.local>
2022-12-05 12:34:15 -0500

    updated user schema. add email, password validator, Timestamp, generate token.

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,9 +1,56 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-const userSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    password: { type: String },
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: [true, 'email must be unique'],
+            lowercase: true,
+            validate(emailValue) {
+                if (!validator.isEmail(emailValue)) {
+                    throw new Error('Email is invalid');
+                }
+            },
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            validate(passwordValue) {
+                if (passwordValue.toLowerCase().includes('password')) {
+                    throw new Error('Password cannot contain password');
+                }
+                if (!validator.isStrongPassword(passwordValue)) {
+                    throw new Error('Password need to be minimum 10 character and contains at least 1 lower character, uppercase, symbol'); // TODO: customize this
+                }
+            },
+        },
+        role: { type: String, enum: ['admin', 'subscriber'] },
+        tokens: [{ token: { type: String, required: true } }],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Used in create user, create
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    // sign() takes two args: data to include into the token and secret
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+    user.tokens = [...user.tokens, { token }];
+    await user.save();
+    return token;
+};
+
+// TODO: hash password, set unique email message in .pre save middleware
+userSchema.pre('save', async function (next) {
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
--- api/src/routers/user.js
+++ api/src/routers/user.js
@@ -3,12 +3,14 @@ const router = new express.Router();
 const User = require('../models/user');
 
 router.post('/users', async (req, res) => {
-    console.log(req.body);
-    const user = new User(req.body);
     try {
+        const user = new User(req.body);
         await user.save();
-        res.send('this is response');
-    } catch (error) {}
+        const token = await user.generateAuthToken(); // .generateAuthToken() is custom function using jwt and it's defined in model. Token will be stored to the user
+        res.status(201).send({ user, token });
+    } catch (error) {
+        res.status(400).send({ error });
+    }
 });
 
 module.exports = router;
