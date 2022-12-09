85c919961377a62bc3f3d7014029e3c4ecbf1c63
Aya Y <ayay@Ayas-MacBook-Pro.local>
2022-12-09 17:06:09 -0500

    create test for create user

--- api/tests/fixtures/db.js
+++ api/tests/fixtures/db.js
@@ -1,8 +1,15 @@
 const User = require('../../src/models/user');
 
+const UserOne = {
+    name: 'User one',
+    email: 'userone@test.ca',
+    password: 'Testtest123!!',
+};
+
 // Function for Wiping DB for each testing
 const setupDatabase = async () => {
     await User.deleteMany();
+    await new User(UserOne).save();
 };
 
-module.exports = { setupDatabase };
+module.exports = { setupDatabase, UserOne };
--- api/tests/user.test.js
+++ api/tests/user.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
-const { setupDatabase } = require('./fixtures/db');
+const { setupDatabase, UserOne } = require('./fixtures/db');
 
 // Wiping DB and setup variable
 beforeEach(setupDatabase);
@@ -16,15 +16,62 @@ test('User create successfully. The response should contain token', async () => 
             password: 'Testtest123!!',
         })
         .expect(201);
+    // DB check
     const user = await User.findById(response.body.user._id);
+    expect(user.name).toEqual('Ayaka');
+    expect(user.email).toEqual('test@test.ca');
+
+    // Response check
+    expect(response.body).toMatchObject({
+        user: { name: 'Ayaka', email: 'test@test.ca' },
+        token: user.tokens[0].token,
+    });
+    // To make sure password is not plain text
+    expect(user.password).not.toEqual('Testtest123!!');
 });
 
 // user should not be created without name
+test('user should not be created without name', async () => {
+    await request(app)
+        .post('/users')
+        .send({
+            email: 'test@test.ca',
+            password: 'testtest',
+        })
+        .expect(400);
+});
 // user should not be created without email
+test('user should not be created without email', async () => {
+    await request(app)
+        .post('/users')
+        .send({
+            name: 'Ayaka',
+            password: 'Testtest123!!',
+        })
+        .expect(400);
+});
 // user should not be created with simple email
+test('user should not be created without email', async () => {
+    await request(app)
+        .post('/users')
+        .send({
+            name: 'Ayaka',
+            password: 'Testtest123!!',
+        })
+        .expect(400);
+});
 
-// user should not be created without password
 // user should not be created with duplicated email
+test('user should not be created with duplicated email', async () => {
+    await request(app)
+        .post('/users')
+        .send({
+            name: 'Ayaka',
+            email: UserOne.email,
+            password: 'Testtest123!!',
+        })
+        .expect(400);
+});
 
 // user should login successfully
 // user should not login with wrong password
