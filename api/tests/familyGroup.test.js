e331977757b2bd116439096ccf3f42198b9386ff
Aya Y <ayay@Ayas-MacBook-Pro.local>
2022-12-12 12:05:27 -0500

    add testing plan

--- api/tests/familyGroup.test.js
+++ api/tests/familyGroup.test.js
@@ -1,5 +1,31 @@
+const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/models/user');
+const FamilyGroup = require('../src/models/familyGroup');
+
+const { setupDatabase, UserOne } = require('./fixtures/db');
+
+// Wiping DB and setup variable
+//beforeEach(setupDatabase);
+
+// Family should be created successfully and the user should be assigned as the owner and members
 test('family should be created successfully and the user should be assigned as the owner and members', () => {});
 
-// family group
+// family group should not be created without authorization
+// family group should not be created without name property
 
 // should fetch family group if the user is in the group
+// should not fetch family group if the user is not in the group
+
+// should delete family group if it's owner
+// should NOT delete family group if user is not the owner, it's member
+// should NOT delete family group if user is not log in
+
+// should update family group if user is member and valid field
+// should NOT update family group if user is not member
+// should NOT update family group if user is not login
+// should NOT update family group if it's invalid filed
+
+// should add family member if user is member
+// should NOT add family member if user is not member
+// should NOT add family member if user is not login
--- api/tests/recipe.test.js
+++ api/tests/recipe.test.js
@@ -1,3 +1,16 @@
+// Recipe should be created successfully and the user should be assigned as the owner and member
 test('recipe should be created successfully and the user should be assigned as the owner', () => {});
 
+// Recipe should NOT be created without name property
+// Recipe should NOT be created without VALID familyGroupIds property
+// Recipe should NOT be created without login
+
 // should fetch recipe if the user is in the group
+// should NOT fetch recipe if the user is NOT in the group
+// should NOT fetch recipe if the user is NOT login
+// should NOT fetch recipe if it's wrong /:id
+
+// should fetch recipe if the user is in the group
+// should NOT fetch recipe if the user is NOT in the group
+// should NOT fetch recipe if the user is NOT login
+// should NOT fetch recipe if it's wrong familyGroup Id
