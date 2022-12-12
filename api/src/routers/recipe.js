e25bba150b0198ca06ff484965703000cb4761da
Aya Y <ayay@Ayas-MacBook-Pro.local>
2022-12-12 16:35:10 -0500

    update endpoint for recipe. add familygroup to recipe

--- api/src/routers/recipe.js
+++ api/src/routers/recipe.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const router = new express.Router();
+const mongoose = require('mongoose');
 const Recipe = require('../models/recipe');
+const FamilyGroup = require('../models/familyGroup');
 const checkFamilyGroup = require('../helper/checkFamilyGroup');
 
 // Routers
 router.post('/recipes', auth, async (req, res) => {
     try {
         const user = req.user;
-        // TODO: make sure user is joined the familyGroup
+
+        // VALIDATE user is joined the recipe
+        const isValidate = checkFamilyGroup(req.familyGroups, req.body.familyGroupIds);
+        if (!isValidate) {
+            return res.status(401).send({ error: 'Please make sure you join the family group' });
+        }
+
         const recipe = new Recipe({ ...req.body, owner: user._id });
         await recipe.save();
-        await recipe.populate('familyGroupIds');
-        await recipe.populate('owner');
+        // await recipe.populate('familyGroupIds');
+        // await recipe.populate('owner');
 
         res.status(201).send(recipe);
     } catch (error) {
@@ -29,7 +37,7 @@ router.get('/recipes/:id', auth, async (req, res) => {
             return res.status(404).send({ error: 'Recipe not found' });
         }
 
-        // VALIDATE user is joined the familyGroup
+        // VALIDATE user is joined the recipe
         const isValidate = checkFamilyGroup(req.familyGroups, recipe.familyGroupIds);
         if (!isValidate) {
             return res.status(404).send({ error: 'Recipe not found. Please make sure joining the family group' });
@@ -45,7 +53,7 @@ router.get('/recipes/:id', auth, async (req, res) => {
 });
 
 // read all joined family group recipes
-router.get('/recipes/familyGroup/:id', auth, async (req, res) => {
+router.get('/recipes/recipe/:id', auth, async (req, res) => {
     try {
         const familyGroupId = req.params.id;
 
@@ -64,5 +72,102 @@ router.get('/recipes/familyGroup/:id', auth, async (req, res) => {
     }
 });
 // TODO: TBI read specific joined family group recipes
+
+// update
+router.patch('/recipes/:id', auth, async (req, res) => {
+    const allowedProperties = ['name', 'portions', 'ingredients', 'steps', 'note', 'externalUrl'];
+    const requestedProperties = Object.keys(req.body);
+
+    const isPropertyAllow = requestedProperties.every((property) => allowedProperties.includes(property));
+
+    if (!isPropertyAllow) {
+        return res.status(400).send({ error: 'The property not allow to update' });
+    }
+
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).send({ error: 'Recipe not found' });
+        }
+
+        // VALIDATE user is joined the recipe
+        const isValidate = checkFamilyGroup(req.familyGroups, recipe.familyGroupIds);
+        if (!isValidate) {
+            return res.status(404).send({ error: 'Recipe not found. Please make sure joining the family group' });
+        }
+
+        // TODO: make sure ingredients and steps update work
+        requestedProperties.forEach((property) => {
+            recipe[property] = req.body[property];
+        });
+        await recipe.save();
+        res.send(recipe);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.patch('/recipes/:id/familyGroup', auth, async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        const newFamilyGroup = await FamilyGroup.findOne({ _id: req.body['familyGroup'] });
+
+        // VALIDATE user is joined the recipe
+        const isValidate = checkFamilyGroup(req.familyGroups, recipe.familyGroupIds);
+        if (!isValidate) {
+            return res.status(404).send({ error: 'Recipe not found. Please make sure joining the family group' });
+        }
+
+        // VALIDATING IF RECIPE EXIST AND USER IS AUTHORIZED
+        if (!recipe) {
+            return res.status(404).send({ error: 'Recipe not Found' });
+        }
+        // VALIDATING IF THE NEW FAMILY GROUP EXIST
+        if (!newFamilyGroup) {
+            return res.status(404).send({ error: 'Family Group Not Found' });
+        }
+        // VALIDATING IF THE USER ALREADY IN MEMBERS
+        const isNewFamilyExist = recipe.familyGroupIds.every((familyGroupId) => {
+            // console.log(JSON.stringify(familyGroupId), JSON.stringify(newFamilyGroup._id));
+            return JSON.stringify(familyGroupId) !== JSON.stringify(newFamilyGroup._id);
+        });
+
+        if (!isNewFamilyExist) {
+            return res.status(400).send({ error: 'This family group is already added' });
+        }
+
+        // UPDATING DB
+        console.log(new mongoose.mongo.ObjectId(newFamilyGroup._id));
+        recipe.familyGroupIds = [...recipe.familyGroupIds, new mongoose.mongo.ObjectId(newFamilyGroup._id)];
+
+        await recipe.save();
+
+        // SEND DATA TO FRONTEND
+        await recipe.populate('familyGroupIds'); // Converting userIDs to name/email etc
+        res.send(recipe);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// delete
+router.delete('/recipes/:id', auth, async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).send({ error: 'Recipe not found' });
+        }
+
+        // VALIDATE user is joined the recipe
+        const isValidate = checkFamilyGroup(req.familyGroups, recipe.familyGroupIds);
+        if (!isValidate) {
+            return res.status(404).send({ error: 'Recipe not found. Please make sure joining the family group' });
+        }
+        await recipe.remove();
+        res.send(recipe);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
 
 module.exports = router;
