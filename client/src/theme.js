b65105f9f61f8f1b8defbbc662b961ad2bec88b9
Aya Y <ayay@Ayas-MacBook-Pro.local>
2022-12-13 11:11:38 -0500

    install react app. clean up. add styled component

---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {},
+    spacing: {
+        xs: '8px',
+        s: '16px',
+        m: '24px',
+        l: '32px',
+    },
+    borderRadius: '5px',
+};
+
+export default theme;
